51a7b8db789ba50fcd12de67a92c109403750a9d
瑞秋A <1209343515@qq.com>
2016-11-17 12:49:37 +0800

    Create location_replace.js

---
+++ 2 js/location_replace.js
@@ -0,0 +1,12 @@
+ //var currLocation = location.href;
+        //var target = "";
+        //
+        //if (gIdReg.test(location)) {
+        //    target = location.search.replace(gIdReg, function (a, b, c, d) {
+        //        return b + c + gid;
+        //    });
+        //} else {
+        //    target = currLocation.indexOf('?') > -1 ? (currLocation + "&g=" + gid) : (currLocation + "?g=" + gid);
+        //}
+        //
+        //location.href = target;
